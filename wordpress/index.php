a2bec172be93de3cd16b1cd4f6a0c26f114fb770
dgerardomartinez <dgerardo.martinez@gmail.com>
2022-06-26 19:41:56 -0600

    actualizado el index

--- wordpress/index.php
+++ wordpress/index.php
@@ -3,6 +3,19 @@
   <title>Examen final Devops</title>
  </head>
  <body>
- <?php echo '<p>EXAMEN FINAL DEVOPS 2022</p>'; ?>
+ <div align= "center">
+<strong>
+<font face="Arial" size="20"> <?php echo '<p>EXAMEN FINAL 2022</p>'; ?>
+</font> 
+<font face="Arial" size="20">  <?php echo '<p>UNIVERSIDAD GALILEO</p>'; ?>
+</font>
+<font face="Arial" size="20">  <?php echo '<p>DEVOPS</p>'; ?>
+</font>
+</strong>
+ <img src="galileo.jpg"
+ width="800"
+ height="341"
+ </div
+ >
  </body>
 </html>
